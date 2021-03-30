817ff564dd67d733d8333ae74d795d04dc5572f8
i develop things <ocawesome101@gmail.com>
2021-03-30 13:28:21 -0400

    fancy button wheeeeee

--- utils/uibase.lua
+++ utils/uibase.lua
@@ -17,12 +17,35 @@ touch.init("/dev/input/event1", UI_WIDTH, UI_HEIGHT)
 text.load_font("font/font.bin")
 
 local home = ui.button.new((UI_WIDTH//2) - 60*UI_SCALE, UI_HEIGHT - 36*UI_SCALE,
-  120*UI_SCALE, 36*UI_SCALE, "[]", 0xFFFFFF, 0x000000, UI_SCALE)
-local back = ui.button.new()
+  120*UI_SCALE, 36*UI_SCALE, "", 0x000000, 0x111111, UI_SCALE)
+local back = ui.button.new(1, UI_HEIGHT - 36*UI_SCALE, 120*UI_SCALE, 36*UI_SCALE, "", 0x000000, 0x111111, UI_SCALE)
+
+function home:tap()
+end
+
 local function draw_base_ui()
 	fb.fill_area(1, UI_HEIGHT - 36*UI_SCALE, UI_WIDTH, 36*UI_SCALE, 0x111111)
   home:refresh(1, 1)
+	back:refresh(1, 1)
 end
 
 fb.fill_screen(0)
 draw_base_ui()
+
+while true do
+	draw_base_ui()
+	local e, x, y = touch.pull_event()
+	if x >= home.x and x <= home.x + home.w
+			and y >= home.y and y <= home.y + home.h then
+		if e == "touch" then
+			home.bg = 0x555555
+			home:refresh(1,1)
+			home.down = true
+		elseif e == "drop" and home.down then
+			home.bg = 0x111111
+			home.down = false
+			home:refresh(1,1)
+			home:tap()
+		end
+	end
+end
