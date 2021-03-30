1b7869c6393b747a7dbc09732652def32c1c32f5
i develop things <ocawesome101@gmail.com>
2021-03-30 12:42:33 -0400

    receiving SMS is now possible.  yay!

--- lib/modem.lua
+++ lib/modem.lua
@@ -27,17 +27,24 @@ function lib.read_message()
 	return modem:read(64, 500)
 end
 
-function lib.send_command(cmd)
+function lib.send_command(cmd, ...)
 	chinit()
+	if #({...}) > 0 then
+	  cmd = string.format(cmd, ...)
+	end
 	modem:write(cmd .. "\r")
 	print(cmd)
-	local resp = modem:read(1024, 500)
+	local resp = modem:read(math.huge, 500)
+	if resp:match("ERROR") then
+		return nil, resp
+	end
 	print(resp)
 	return resp
 end
 
 -- SMS is currently implemented in text mode.  This is much simpler than PDU
--- mode.
+-- mode.  However, on the PinePhone's modem, there is no way to send Unicode
+-- in text mode.
 -- 'num' must be a full phone number with the '+' omitted, ex. +13459871234
 -- becomes 13459871234.
 function lib.send_sms(num, message_text)
@@ -63,9 +70,61 @@ end
 local sms_header_pattern = "%+CMGL: (%d+),(\"REC U?N?READ\"),\"%+(%d+)\",,\"(.+)\""
 function lib.poll_sms()
 	-- step 1: List all the available messages
+	local messages = lib.send_command("AT+CMGL=\"ALL\"")
+	-- step 2: Split the list into lines for ease of parsing
+	local lines = {}
+	do
+		local line = ""
+		for c in messages:gmatch(".") do
+			if c == "\n" then
+				lines[#lines + 1] = line
+				line = ""
+			elseif c ~= "\r" then
+				line = line .. c
+			end
+		end
+		if #line > 0 then lines[#lines + 1] = line end
+	end
+	-- step 3: Parse the list, line-by-line
+	local list = {}
+	local index, status, from, time, data = nil, nil, nil, nil, ""
+	for i, v in ipairs(lines) do
+		if v == "" and lines[i + 1] == "OK" then
+			if #data > 0 then
+				list[index] = {
+					from = from,
+					time = time,
+					data = data,
+					status = status}
+			end
+			-- Cave Johnson, we're done here.
+			return list
+		elseif v:match(sms_header_pattern) then
+			local I, S, F, T = v:match(sms_header_pattern)
+			if index then
+				list[index] = {
+					from = from,
+					time = time,
+					data = data,
+					status = status}
+			end
+			index = tonumber(I)
+			status = S
+			from = "+" .. F
+			time = T
+			data = ""
+		else
+			data = data .. v .. "\n"
+		end
+	end
 end
 
-function lib.pop_sms()
+function lib.delete_sms(index)
+	-- Mark the message as read.  I'm not sure if this is necessary.
+	lib.send_command("AT+CMGR=%d", index)
+	-- Delete the message from the SMS queue.
+	lib.send_command("AT+CMGD=%d", index)
+	return true
 end
 
 return lib
---
+++ modem_recv.lua
@@ -0,0 +1,22 @@
+-- Recieve texts.
+
+local md = require("lib/modem")
+
+md.init("/dev/ttyUSB2")
+
+local list = md.poll_sms()
+
+if not list then
+	print("No messages in queue.")
+	return
+end
+for i, v in pairs(list) do
+	print("\nMessage index: " .. i)
+	print("  Sender: " .. v.from)
+	print("  Received at: " .. v.time)
+	print("  Message status: " .. v.status)
+	print("  Message data: " .. v.data)
+	if ({...})[1] == "d" then
+		md.delete_sms(i)
+	end
+end
